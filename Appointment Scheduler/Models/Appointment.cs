a8e67d04da073eb67295e8101bd4d0c391ec74e1
SoundouceCHI <soundouce.chibani@gmail.com>
2024-01-10 22:45:14 +0100

    Add models, DbContext, and create initial migration

---
+++ Appointment Scheduler/Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Appointment_Scheduler.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Appointment Scheduler/Models/AppointmentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Appointment_Scheduler.Models
+{
+    public class AppointmentContext : DbContext
+    {
+        public DbSet<Appointment> appointments { get; set; }
+        public DbSet<UserApp> users { get; set; }                                             
+
+        public AppointmentContext(DbContextOptions<AppointmentContext> options) : base(options) {
+        
+        }
+    }
+}
